b2d1811d685a36920fc8331cdcc27eae5953a72a
Maria Cecilia <ceciandres2014@gmail.com>
2026-04-07 23:54:57 -0500

    maria_ospina

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const repoName = 'Maria-Cecilia-Ospina';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: `/${repoName}`
+		}
 	}
 };
 
