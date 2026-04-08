6c5efcd69b7b8607621d33c5cca0ec227c7d8baa
Maria Cecilia <ceciandres2014@gmail.com>
2026-04-08 18:21:51 -0500

    chore: CI/CD optimization for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 
-const repoName = 'Maria-Cecilia-Ospina';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html'
+			fallback: 'index.html',
+			precompress: false
 		}),
 		paths: {
-			base: `/${repoName}`
+			base: '/Maria-Cecilia-Ospina'
+		},
+		prerender: {
+			entries: ['/']
 		}
 	}
 };
